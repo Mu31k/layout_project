69b8ee213af68bdb1fbdd5e99edc75005ca9dc76
Muzikir <muzikir1217@yandex.ru>
2021-12-28 12:26:18 +0500

    Initial commit

---
+++ mysite/polls/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'polls/index.html')
+
+
+def about(request):
+    return render(request, 'polls/about.html')
